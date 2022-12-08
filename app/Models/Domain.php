d251d598f27ec4473729fdb0c71adb142f7f8f90
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2022-12-09 02:26:21 +0500

    Domain Model extend Stancl Domain Model

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Stancl\Tenancy\Database\Models\Domain as ModelsDomain;
 
-class Domain extends Model
+class Domain extends ModelsDomain
 {
     use HasFactory;
 }
