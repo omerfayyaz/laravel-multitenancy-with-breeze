a05525775acea6cb59967b9970a1c8b75bfc1cf8
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2022-12-10 00:22:15 +0500

    Move Routes to tenant.php

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,6 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        'localhost',
         'tenancy-with-breeze.test',
     ],
 
