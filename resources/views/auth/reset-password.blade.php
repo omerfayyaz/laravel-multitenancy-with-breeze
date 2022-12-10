c6c947f72ea52a37c1e674734182b553df3f7c10
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2022-12-11 00:38:09 +0500

    add tenant. to all tenant route names

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -6,7 +6,7 @@
             </a>
         </x-slot>
 
-        <form method="POST" action="{{ route('password.store') }}">
+        <form method="POST" action="{{ route('tenant.password.store') }}">
             @csrf
 
             <!-- Password Reset Token -->
