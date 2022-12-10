cd1e3c9eb4db18caaadec9afde26ed767a505bbb
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2022-12-10 22:34:47 +0500

    Move Password encryption from User to Tenant Model

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -13,4 +13,13 @@ class Tenant extends ModelsTenant implements TenantWithDatabase
     use HasFactory;
     use HasDatabase;
     use HasDomains;
+
+    public static function booted() {
+
+        static::creating(function ($tenant) {
+
+            $tenant->password = bcrypt($tenant->password);
+
+        });
+    }
 }
